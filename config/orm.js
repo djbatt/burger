ca9bcdefcd7b442e128e2589916e7b01a54bfe06
djbatt <djbatt@live.com>
2018-08-11 11:07:36 -0400

    added some more

--- config/orm.js
+++ config/orm.js
@@ -2,13 +2,18 @@ var connection = require("../config/connection.js");
 
 var orm = {
     selectAll: function() {
+        connection.query("SELECT * FROM burgers", function(err, result) {
+            if (err) throw err;
+            res.json(result);
+        })
 
     },
     insertOne: function() {
+        connection.query(`INSERT INTO burgers`)
 
     },
     updateOne: function() {
-        
+
     }
 }
 

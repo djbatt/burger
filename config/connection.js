24731ac56382ee34a0d805d7d222eb5f589422fb
djbatt <djbatt@live.com>
2018-08-16 16:19:17 -0400

    added jawsdb url

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "nd5o0u6jd45esccb",
-    password: "sjw33wz6g9xs1k02",
-    database: "ht8cymf3yv8l40dl"
-})
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    })
+}
 
 connection.connect(function(err) {
     if (err) {

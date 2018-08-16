4310f5eea1546451f68ecd0e2f7bf9d56f8f7b1e
djbatt <djbatt@live.com>
2018-08-16 16:12:36 -0400

    connection edit

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,5 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "	jzejgx94iys0q74o",
-    password: "di613xbhh6616e9t",
-    database: "sqjpz6rfxd9hw2yh"
-})
-
 connection.connect(function(err) {
     if (err) {
         console.log(`Error connecting ${err.stack}`);

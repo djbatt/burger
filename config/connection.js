7bd02478ecec77e1f6bfd7c95211efd9a6861c5c
djbatt <djbatt@live.com>
2018-08-16 16:03:56 -0400

    conection changes

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
+    user: "	jzejgx94iys0q74o",
+    password: "di613xbhh6616e9t",
+    database: "sqjpz6rfxd9hw2yh"
 })
 
 connection.connect(function(err) {
